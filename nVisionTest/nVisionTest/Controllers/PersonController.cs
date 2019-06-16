c8f27e58fc17bec5bcc23213db7c3bfee30d1ff8
Ricardo Gamboa <ricardogamboa@live.com>
2019-06-16 12:46:51 -0600

    Project creation

---
+++ nVisionTest/nVisionTest/Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using nVisionTest.Models;
+
+namespace nVisionTest.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public bool Index([Bind("FirstName,LastName,Gender,Email,Phone,Country")] Person person)
+        {
+            try
+            {
+                System.Xml.Serialization.XmlSerializer writer =
+                    new System.Xml.Serialization.XmlSerializer(typeof(Person));
+                TextWriter textWriter = new StreamWriter(Environment.CurrentDirectory + "//DataBase.xml", true);
+                writer.Serialize(textWriter, person);
+                textWriter.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ nVisionTest/nVisionTest/wwwroot/js/Person.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(
+    $("#btnSubmit").click(function () {
+        if (Validate()) {
+            $.ajax({
+                type: "POST",
+                url: "/Person/Index",
+                data: {
+                    person: {
+                        FirstName: $("#txtFirstName").val(),
+                        LastName: $("#txtLastName").val(),
+                        Gender: $("input[type=radio]:checked").val(),
+                        Email: $("#txtEmail").val(),
+                        Phone: $("#txtPhone").val(),
+                        Country: $("option:selected").text()
+                    }
+                },
+                success: function (data)
+                {
+                    if (data) {
+                        alert("validated sign up");
+                        location.reload();
+                    }
+                    else
+                        alert("Unknown error. Please try again in a while!");
+                },
+                error: function () { alert("Error"); }
+            });
+        }
+    })
+);
+
+function Validate() {
+    var access = true;
+    $("input[type=text]").each(function () {
+        if ($(this).val().trim() == "") {
+            access = false;
+            $("#lbl" + $(this).attr("id")).show();
+        } else {
+            $("#lbl" + $(this).attr("id")).hide();
+        }
+    });
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test($("#txtEmail").val())) {
+        access = false;
+        $("#lbltxtEmail").show();
+    }
+    return access;
+}
+
+$('#txtPhone').keypress(function (event) {
+
+    if (event.which != 8 && isNaN(String.fromCharCode(event.which))) {
+        event.preventDefault();
+    }
+})
